adac13bde6a09d5bee42b3a8cebef7ef2cdd4197
Adrian Juszczak <adrian.juszczak@datawalk.com>
2021-02-14 14:21:33 +0100

    canvas: basics

---
+++ scripts/canvas.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+context.fillStyle = 'rgba(0, 200, 0, 0.5)';
+context.strokeStyle = '#ff0000';
+context.lineWidth = 5;
