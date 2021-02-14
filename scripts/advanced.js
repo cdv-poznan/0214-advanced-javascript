5195b257e291c25d43f50c5cd23425e1f4e9bc26
Adrian Juszczak <adrian.juszczak@datawalk.com>
2021-02-14 15:26:08 +0100

    file upload

--- scripts/advanced.js
+++ scripts/advanced.js
@@ -1,6 +1,24 @@
-
+/*
 localStorage.setItem('username', 'Adrian');
 
 var username = localStorage.getItem('username');
 
 console.log(username);
+*/
+
+var fileInput = document.querySelector('#upload');
+var files = document.querySelector('#files');
+
+fileInput.addEventListener('change', function(event) {
+  console.log(event);
+  console.log(event.target.files);
+  var file = event.target.files[0]; // File
+  var url = URL.createObjectURL(file); // string z tymczasowym adresem URL
+  console.log(url);
+
+  var img = document.createElement('img');
+  img.src = url;
+  img.style.maxWidth = '100px';
+
+  files.append(img);
+});
